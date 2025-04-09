98ed1c84f1c9ac81652bd5d44f93e3c62e36b57d
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-04-09 16:27:30 -0600

    feat: add capacitor assets dependency and update home page buttons
    - Added "@capacitor/assets" dependency to package.json.
    - Updated home.page.html to include IDs for the increment and decrement buttons.
    - Enhanced home.page.spec.ts to test the functionality of the increment button and verify the counter's behavior.

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePage } from './home.page';
+import { By } from '@angular/platform-browser';
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -12,7 +13,11 @@ describe('HomePage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('change num', () => {
+    const buttonUp = fixture.debugElement.query(By.css('#button_up'));
+    const buttonDown = fixture.debugElement.query(By.css('#button_down'));
+    buttonUp.nativeElement.click();
+    expect(component.counter).toBe(1);
+    expect(component.showNumber).toBe('01');
   });
 });
